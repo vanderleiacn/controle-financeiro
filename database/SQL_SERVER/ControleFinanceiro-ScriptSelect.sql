67a79fe02cd54815f23b4f79b78be500fb06f659
vanderleiacn <vanderleiacn@gmail.com>
2022-06-15 14:09:02 -0300

    incluindo script de select

---
+++ database/SQL_SERVER/ControleFinanceiro-ScriptSelect.sql
@@ -0,0 +1,11 @@
+USE controleFinanceiro
+
+
+SELECT * FROM usuario
+SELECT * FROM cliente
+SELECT * FROM tipoCliente
+SELECT * FROM tipoPlanoCliente
+SELECT * FROM tipoLancamento
+SELECT * FROM categoriaLancamento
+SELECT * FROM tipoMeioPagamento
+SELECT * FROM Lancamento
